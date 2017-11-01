a5921adccb8ca275d416470dae1f68b57027d0d8
Johannes Hölzl <johannes.hoelzl@gmx.de>
2017-11-01 15:52:05 +0100

    fix

--- mason_stother.lean
+++ mason_stother.lean
@@ -1,9 +1,4 @@
 -- the to be Mason-Stother formalization
-<<<<<<< HEAD
---Authors Johannes & Jens
-=======
--- Authors Johannes and Jens
->>>>>>> d8ea8277487368a6e3f34fb26c245e3449c347ed
-
+-- Authors Johannes & Jens
 
 
