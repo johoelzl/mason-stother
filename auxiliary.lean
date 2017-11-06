77908b994f0477cb092efc4ceafe0d325bd10be2
Jens Wagemaker <jens.wagemaker@gmail.com>
2017-11-06 23:32:08 +0100

    Defined addition, and showed that polynomials are closed under addition.

---
+++ auxiliary.lean
@@ -0,0 +1,18 @@
+universe u
+variables {α : Type u} [decidable_linear_order α]
+
+lemma right_le {a b c : α} (h :(max a b) ≤ c) : b ≤ c := 
+have h0 : ¬b > c, from 
+  (assume h1: b > c,
+  have h2: (max a b ) ≥ b, from (le_max_right a b),
+  have h3: c < b, from h1,
+  have h4: b ≤ (max a b), from h2,
+  have h5: c < (max a b), from lt_of_lt_of_le h3 h4,
+  have h6: (max a b) > c, from h5,
+  have h7: ¬((max a b) ≤ c), from not_le_of_gt h6,
+  show false, from (h7 h)),
+show b≤c, from  le_of_not_gt h0
+
+lemma left_le {a b c : α} (h :(max a b) ≤ c) : a ≤ c := 
+have h0: (max b a) ≤ c, from (max_comm a b) ▸ h,
+show a ≤ c, from right_le h0
--- polynomial.lean
+++ polynomial.lean
@@ -23,39 +23,108 @@ Polynomials and their ring structure
 
 -/
 
+
+import .auxiliary
+
+
 structure polynomial (α : Type) [ring α] : Type :=
 (to_fun : ℕ → α)
 (finite : ∃n:ℕ, ∀m≥n, to_fun m = 0)
 
 section
+
 variables {α : Type} [ring α]
 
 instance : has_zero (polynomial α) :=
-⟨⟨(λn, 0), _⟩⟩
+⟨⟨(λn, 0), ⟨0, assume (n : ℕ) _, rfl⟩⟩⟩
 
-def pC (a : α) : ℕ → α
+private def pC (a : α) : ℕ → α
 | 0 := a
 | _ := 0
 
-lemma finite_pC' {a : α} : ∀m:ℕ, m ≥ 1 → pC a m = 0 :=
-_
+private lemma finite_pC {a : α} : ∀m:ℕ, m ≥ 1 → pC a m = 0
+| 0 h := absurd h dec_trivial
+| (n + 1) h := rfl
 
-lemma finite_pC {a : α} : ∃n:ℕ, ∀m≥n, pC a m = 0 :=
-_
+def C (a : α) : polynomial α := ⟨pC a, 1, finite_pC⟩
 
-def C₀ (a : α) : polynomial α := ⟨pC a, finite_pC⟩
+private def fun_X (n : ℕ) : α := if n = 1 then 1 else 0
 
-def C₁ (a : α) : polynomial α :=
-⟨(λn, match n with 0 := a | _ := 0 end), _⟩
+private lemma finite_X : ∀m:ℕ, m ≥ 2 → fun_X m = (0 : α)
+| 0 h := absurd h dec_trivial
+| 1 h := absurd h dec_trivial
+| (n + 2) h := eq.refl (fun_X (n + 2))
 
-def C₂ (a : α) : polynomial α :=
-⟨(λn, if n = 0 then a else 0), _⟩
+def X : polynomial α := ⟨fun_X, 2, finite_X⟩
 
-def X : polynomial α :=
-⟨(λn, if n = 1 then 1 else 0), _⟩
+def fun_add (p q : polynomial α) (n : ℕ) : α := p.to_fun n + q.to_fun n
 
-instance : has_add (polynomial α) :=
-⟨λp q, ⟨λn, p.to_fun n + q.to_fun n, _⟩⟩
+lemma finite_fun_add : ∀ (p q : polynomial α), ∃n:ℕ, ∀m≥n, (fun_add p q) m = (0 : α) 
+| ⟨f, n, hf⟩ ⟨g, m, hg⟩ := ⟨max n m, assume i hi, 
+    have hin: i ≥ n, from le_trans (le_max_left _ _) hi, 
+    have him: i ≥ m, from le_trans (le_max_right _ _) hi,
+    by simp [fun_add, hin, him, hf, hg]⟩   
+
+instance : has_add (polynomial α) := 
+⟨λp q, ⟨fun_add p q, finite_fun_add p q⟩⟩
+
+lemma pol_zero_add_fun : ∀ a : (polynomial α ), (0 + a).to_fun = a.to_fun:= 
+assume a : polynomial α, 
+have h0: 0 + a = 0 + a, from eq.refl _,
+
+have h2: ∀n:ℕ, fun_add 0 a n = a.to_fun n, from 
+assume n:ℕ, zero_add (a.to_fun n),  
+show fun_add 0 a = a.to_fun, from funext h2
+
+
+lemma pol_zero_add_finite : ∀ a : (polynomial α), (∃n:ℕ, ∀m≥n, (((0 +  a).to_fun m) = (0 : α))) = (∃n:ℕ, ∀m≥n, ((a.to_fun m) = (0 : α))):= 
+assume a : (polynomial α),
+suffices h: (∃n:ℕ, ∀m≥n, (((0 + a).to_fun m) = (0 : α))) ↔ (∃n:ℕ, ∀m≥n, ((a.to_fun m) = (0 : α))), from propext h,
+⟨
+  assume h1, match h1 with 
+  |⟨number,hn⟩:= ⟨number, assume i hi,
+   have h2:fun_add 0 a i = 0, from hn i hi,have h3:fun_add 0 a = a.to_fun, from pol_zero_add_fun a, 
+   h3 ▸ h2
+   ⟩  
+  end
+
+, assume h1, match h1 with 
+  |⟨number,hn⟩:= ⟨number, assume i hi,
+   have h2: a.to_fun i = 0, from hn i hi,have h3:fun_add 0 a = a.to_fun, from pol_zero_add_fun a, 
+   have h4: a.to_fun = fun_add 0 a, from eq.symm h3,
+   @eq.subst _ (λ x : ℕ → α , (x i = 0) ) a.to_fun (fun_add 0 a) h4 h2
+   ⟩  
+  end⟩  
+
+lemma pol_zero_add_finite_iff : ∀ a : (polynomial α), (∃n:ℕ, ∀m≥n, (((0 +  a).to_fun m) = (0 : α))) ↔  (∃n:ℕ, ∀m≥n, ((a.to_fun m) = (0 : α))):= 
+assume a : (polynomial α),
+⟨
+  assume h1, match h1 with 
+  |⟨number,hn⟩:= ⟨number, assume i hi,
+   have h2:fun_add 0 a i = 0, from hn i hi,have h3:fun_add 0 a = a.to_fun, from pol_zero_add_fun a, 
+   h3 ▸ h2
+   ⟩  
+  end
+
+, assume h1, match h1 with 
+  |⟨number,hn⟩:= ⟨number, assume i hi,
+   have h2: a.to_fun i = 0, from hn i hi,have h3:fun_add 0 a = a.to_fun, from pol_zero_add_fun a, 
+   have h4: a.to_fun = fun_add 0 a, from eq.symm h3,
+   @eq.subst _ (λ x : ℕ → α , (x i = 0) ) a.to_fun (fun_add 0 a) h4 h2
+   ⟩  
+  end⟩  
+
+#check λ a :(polynomial α), (∃n:ℕ, ∀m≥n, (((a + 0) m)= (0 : α))) 
+
+lemma pol_zero_add2 : ∀ a : (polynomial α ), a = 0 + a
+|⟨f, i, hi⟩ := 
+have h: (⟨f, i, hi⟩: polynomial α ) = ⟨f, i, hi⟩, from rfl,
+have h2: exists.intro i hi =  (0 + (⟨f, i, hi⟩:polynomial α) ).finite, from _, 
+have h1: (⟨f, i, hi⟩: polynomial α ) = 0 + ⟨f, i, hi⟩ , from _,
+--have h1: (⟨f, i, hi⟩: polynomial α ) = ((0:polynomial α ) + ⟨f, i, hi⟩), from _,
+_ 
+ 
+lemma add_zero : ∀ a : α, a + 0 = a := sorry
 
 /-
 instance : ring (polynomial α) :=
