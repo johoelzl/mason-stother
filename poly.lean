c82b3ebe37f7723b2ef1f593d915083aeb016e80
Johannes Hölzl <johannes.hoelzl@gmx.de>
2017-11-20 17:24:13 +0100

    implement polynomial derivative

--- poly.lean
+++ poly.lean
@@ -1,10 +1,54 @@
-import .finsupp
+import .finsupp order.lattice data.nat.cast
 universes u v w
 
 noncomputable theory
 
-open classical set function finsupp
+open classical set function finsupp lattice
 local attribute [instance] decidable_inhabited prop_decidable
+
+instance {α : Type u} [semilattice_sup α] : is_idempotent α (⊔) := ⟨assume a, sup_idem⟩
+
+namespace finset
+variables {α : Type u} {β : Type w} [semilattice_sup_bot α]
+
+def Sup_fin (s : finset β) (f : β → α) : α := s.fold (⊔) ⊥ f
+
+variables {s s₁ s₂ : finset β} {f : β → α}
+
+@[simp] lemma Sup_fin_empty : (∅ : finset β).Sup_fin f = ⊥ :=
+fold_empty
+
+@[simp] lemma Sup_fin_insert {b : β} : (insert b s : finset β).Sup_fin f = f b ⊔ s.Sup_fin f :=
+fold_insert_idem
+
+@[simp] lemma Sup_fin_singleton {b : β} : ({b} : finset β).Sup_fin f = f b :=
+calc _ = f b ⊔ (∅:finset β).Sup_fin f : Sup_fin_insert
+  ... = f b : by simp
+
+lemma Sup_fin_union : (s₁ ∪ s₂).Sup_fin f = s₁.Sup_fin f ⊔ s₂.Sup_fin f :=
+s₁.induction_on (by simp) (by simp {contextual := tt}; cc)
+
+lemma Sup_fin_mono_fun {g : β → α} : (∀b∈s, f b ≤ g b) → s.Sup_fin f ≤ s.Sup_fin g :=
+s.induction_on (by simp) (by simp [-sup_le_iff, sup_le_sup] {contextual := tt})
+
+lemma le_Sup_fin {b : β} (hb : b ∈ s) : f b ≤ s.Sup_fin f :=
+calc f b ≤ f b ⊔ s.Sup_fin f : le_sup_left
+  ... = (insert b s).Sup_fin f : by simp
+  ... = s.Sup_fin f : by simp [hb]
+
+lemma Sup_fin_le {a : α} : (∀b ∈ s, f b ≤ a) → s.Sup_fin f ≤ a :=
+s.induction_on (by simp) (by simp {contextual := tt})
+
+lemma Sup_fin_mono (h : s₁ ⊆ s₂) : s₁.Sup_fin f ≤ s₂.Sup_fin f :=
+Sup_fin_le $ assume b hb, le_Sup_fin (mem_of_subset_of_mem h hb)
+
+end finset
+
+instance nat.distrib_lattice : distrib_lattice ℕ :=
+by apply_instance
+
+instance nat.semilattice_sup_bot : semilattice_sup_bot ℕ :=
+{ bot := 0, bot_le := nat.zero_le , .. nat.distrib_lattice }
 
 -- TODO: relax semiring to semi-algebra?
 def polynomial (α : Type u) [semiring α] := ℕ →₀ α
@@ -15,6 +59,7 @@ variables {α : Type u} {a a' a₁ a₂ : α} {n m : ℕ}
 section semiring
 variable [semiring α]
 
+instance : has_coe_to_fun (polynomial α) := finsupp.has_coe_to_fun
 instance : has_zero (polynomial α) := finsupp.has_zero
 instance : has_one (polynomial α) := finsupp.has_one
 instance : has_add (polynomial α) := finsupp.has_add
@@ -34,10 +79,6 @@ lemma single_eq_X_pow : ∀{n}, single n a = C a * X ^ n
 | (n + 1) :=
   calc single (n + 1) a = single n a * X : by simp [X, single_mul_single]
     ... = C a * X ^ (n + 1) : by rw [@single_eq_X_pow n]; simp [pow_add]
-
-def eval (p : polynomial α) (a : α) : α := p.sum (λn c, c * a ^ n)
-
-def degree (p : polynomial α) : ℕ := p.support.fold max 0 id
 
 lemma induction_on [semiring α] {M : polynomial α → Prop} (p : polynomial α)
   (M_C : ∀(a : α), M (C a)) (M_X : M X)
@@ -60,6 +101,98 @@ begin
   apply M_sum,
   exact (assume a ha, M_single _ _)
 end
+
+def eval (p : polynomial α) (a : α) : α := p.sum $ λn c, c * a ^ n
+
+def degree (p : polynomial α) : ℕ := p.support.Sup_fin id
+
+lemma le_degree {p : polynomial α} {n : ℕ} (h : p n ≠ 0) : n ≤ degree p :=
+show id n ≤ degree p, from finset.le_Sup_fin (by simp [h])
+
+@[simp] lemma degree_zero : degree (0 : polynomial α) = 0 :=
+have (0 : polynomial α).support = ∅, from support_zero,
+by simp [degree, this]; refl
+
+lemma degree_single {a : α} {n : ℕ} : degree (single n a) ≤ n :=
+calc degree (single n a) = (single n a).support.Sup_fin id : by simp [degree]
+  ... ≤ ({n} : finset ℕ).Sup_fin id : finset.Sup_fin_mono finsupp.support_single_subset
+  ... ≤ _ : by simp
+
+@[simp] lemma degree_X (h : 0 ≠ (1:α)) : degree (X : polynomial α) = 1 :=
+le_antisymm
+  degree_single
+  (le_degree $ show (single (1:ℕ) (1:α) : ℕ →₀ α) 1 ≠ 0, begin simp [h.symm] end)
+
+@[simp] lemma degree_C {a : α} : degree (C a) = 0 :=
+nat.eq_zero_of_le_zero degree_single
+
+lemma degree_add {f g : polynomial α} : degree (f + g) ≤ max (degree f) (degree g) :=
+calc degree (f + g) ≤ (f.support ∪ g.support).Sup_fin id : finset.Sup_fin_mono finsupp.support_add
+  ... = max (degree f) (degree g) : finset.Sup_fin_union
+
+lemma degree_sum {β : Type w} {s : finset β} {f : β → polynomial α} :
+  degree (s.sum f) ≤ s.Sup_fin (λi, degree (f i)) :=
+s.induction_on (by simp; refl) $
+  assume b s hbs ih,
+  calc degree ((insert b s).sum f) = degree (f b + s.sum f) : by simp [hbs]
+    ... ≤ degree (f b) ⊔ degree (s.sum f) : degree_add
+    ... ≤ degree (f b) ⊔ s.Sup_fin (λi, degree (f i)) : sup_le_sup (le_refl _) ih
+    ... = (insert b s).Sup_fin (λi, degree (f i)) : by simp
+
+lemma degree_mul {f g : polynomial α} : degree (f * g) ≤ degree f + degree g :=
+calc degree (f * g) ≤ _ : degree_sum
+  ... ≤ _ : finset.Sup_fin_mono_fun $ assume a ha, degree_sum
+  ... ≤ _ : finset.Sup_fin_le $ assume b₁ hb₁, finset.Sup_fin_le $ assume b₂ hb₂,
+    calc _ ≤ b₁ + b₂ : degree_single
+      ... ≤ degree f + degree g : add_le_add (finset.le_Sup_fin hb₁) (finset.le_Sup_fin hb₂)
+
+def derivative (p : polynomial α) : polynomial α :=
+p.sum (λn a, match n with 0 := 0 | (n + 1) := single n (a * (n + 1)) end)
+
+@[simp] lemma derivative_zero : derivative (0 : polynomial α) = 0 :=
+finsupp.sum_zero_index
+
+lemma derivative_single {n : ℕ} {a : α} :
+  derivative (single n a) = @nat.cases_on (λn, polynomial α) n 0 (λn, single n (a * (n + 1))) :=
+finsupp.sum_single_index $ match n with 0 := rfl | n + 1 := by simp [derivative]; refl end
+
+@[simp] lemma derivative_single_zero {a : α} : derivative (single 0 a) = 0 :=
+derivative_single
+
+@[simp] lemma derivative_single_one {n : ℕ} {a : α} :
+  derivative (single (n + 1) a) = single n (a * (n + 1)) :=
+derivative_single
+
+@[simp] lemma derivative_C {a : α} : derivative (C a) = 0 :=
+by simp [C]; refl
+
+@[simp] lemma derivative_one : derivative (1 : polynomial α) = 0 :=
+derivative_C
+
+@[simp] lemma derivative_X : derivative (X : polynomial α) = 1 :=
+by simp [X]; refl
+
+@[simp] lemma derivative_add {f g : polynomial α} :
+  derivative (f + g) = derivative f + derivative g :=
+finsupp.sum_add_index
+  (assume n, match n with 0 := by simp [derivative] | n + 1 := by simp [derivative]; refl end)
+  (assume n, match n with 0 := by simp [derivative] | n + 1 := by simp [derivative, add_mul] end)
+
+@[simp] lemma derivative_sum {β : Type w} {s : finset β} {f : β → polynomial α} :
+  derivative (s.sum f) = s.sum (λb, derivative (f b)) :=
+begin
+  apply (finset.sum_hom derivative _ _).symm,
+  apply_instance,
+  exact derivative_zero,
+  exact assume x y, derivative_add
+end
+
+/-
+@[simp] lemma derivative_mul {f g : polynomial α} :
+  derivative (f * g) = derivative f * g + f * derivative g :=
+calc _ = _ : derivative_sum
+  ... = _ : begin simp [derivative_sum, finsupp.sum] end
+-/
 
 end semiring
 
