ca336eee17dd0f5530377ec07d64694d995bd553
JWageM <jens.wagemaker@gmail.com>
2018-02-01 23:21:27 +0100

    proceded on converion lemmas

--- to_multiset.lean
+++ to_multiset.lean
@@ -1,8 +1,8 @@
-import data.multiset data.finset
-
-open classical multiset finset
+import data.finsupp .to_finsupp
+open classical 
 local attribute [instance] prop_decidable
-
+noncomputable theory
+local infix ^ := monoid.pow
 universe u
 variable α : Type u 
 namespace multiset
@@ -24,5 +24,163 @@ lemma prod_mul_prod_eq_add_prod [comm_monoid α] {a b : multiset α} : a.prod * 
 begin
   simp,
 end
+
+open finsupp 
+
+def to_finsupp {α : Type u} (m : multiset α) : α →₀ ℕ := 
+(m.map $ λa, single a 1).sum
+
+
+lemma to_finsupp_add {α : Type u} (m n : multiset α) :
+   (m + n).to_finsupp = m.to_finsupp + n.to_finsupp :=
+calc (m + n).to_finsupp = ((m.map $ λa, single a 1) + (n.map $ λa, single a 1)).sum :
+    by rw [← multiset.map_add]; refl
+  ... = m.to_finsupp + n.to_finsupp :
+    by rw [multiset.sum_add]; refl
+
+lemma to_finsupp_prod {α : Type*} [comm_monoid α] (m : multiset α) :
+m.prod = (finsupp.prod (m.to_finsupp) (λ k n, k ^ n) ) := 
+begin
+  apply multiset.induction_on m,
+  {
+    simp [to_finsupp, prod_zero_index], 
+  },
+  {
+    intros a s h1,
+    simp [*, to_finsupp] at *,
+    rw [prod_add_index, prod_single_index],
+    repeat {simp [pow_add]}
+  }
+end
+
+
+
+lemma to_finsupp_support {α : Type u} (m : multiset α) : m.to_finsupp.support = m.to_finset :=
+begin
+  apply multiset.induction_on m,
+  {
+    simp [*, to_finsupp, to_finset] at *,
+    exact rfl,
+  },
+  {
+    intros a s h1,
+    simp [*, to_finsupp, to_finset] at *,
+    apply finset.subset.antisymm,
+    {
+       intros y h2,
+       have h3 : support (single a 1 + sum (map (λ (a : α), single a 1) s)) ⊆ support (single a 1) ∪ support (sum (map (λ (a : α), single a 1) s)),
+       from support_add,
+       have h4 : y ∈ support (single a 1) ∪ support (sum (map (λ (a : α), single a 1) s)),
+       from mem_of_subset h3 h2,
+       rw [h1, finset.mem_union] at h4,
+       cases h4,
+       {
+         rw [support_single_ne_zero] at h4,
+         simp * at *,
+         exact ne.symm zero_ne_one,
+       },
+       {
+         simp,
+         by_cases h5 : (y = a),
+         {
+           simp *,
+         },
+         {
+           have h5 : y ∈ s,
+           {
+             rw ←mem_erase_dup,
+             exact h4,
+           },
+           simp *,           
+         }
+       }
+    },
+    {
+      intros y h2,
+      have h3 : support (single a 1 + sum (map (λ (a : α), single a 1) s)) ⊆ support (single a 1) ∪ support (sum (map (λ (a : α), single a 1) s)),
+      from support_add,
+
+      have h4 : y ∈ support (single a 1) ∪ support (sum (map (λ (a : α), single a 1) s)),
+      {
+        rw [finset.mem_union],
+        by_cases h4 : (y = a),
+        {
+          subst h4,
+          rw [support_single_ne_zero],
+          simp,
+          exact ne.symm zero_ne_one,
+        },
+        {
+          rw h1,
+          have h5 :  y ∈  erase_dup s,
+          {
+            rw mem_erase_dup,
+            have : y ∈ erase_dup (a :: s),
+            exact h2,
+            rw mem_erase_dup at this,
+            simp * at *,
+
+          },
+          simp * at *,
+        }
+      },
+      rw [h1, finset.mem_union] at h4,
+      have h5 :   support (single a 1 + sum (map (λ (a : α), single a 1) s)) =
+      support (single a 1) ∪ support (sum (map (λ (a : α), single a 1) s)),
+      {
+        apply finset.subset.antisymm,
+        {
+          exact h3,
+        },
+        {
+          intros x h5,
+          simp at h5,
+          cases h5,
+          {
+            simp,
+            by_contradiction h6,
+            have : (single a 1).val x = 0,
+            from nat.eq_zero_of_add_eq_zero_right h6,
+            exact h5 this,
+          },
+          {
+            simp,
+            by_contradiction h6,
+            have : (sum (map (λ (a : α), single a 1) s)).val x = 0,
+            from nat.eq_zero_of_add_eq_zero_left h6,
+            exact h5 this,           
+          },
+        }
+      },
+      rw h5,
+      simp * at *,   
+    }
+  },
+
+end
+
+lemma to_finsupp_support_subset {α : Type u} (m : multiset α) : m.to_finsupp.support.val ⊆ m :=
+begin
+  intros x h1,
+  rw to_finsupp_support at h1,
+  rw ← mem_to_finset,
+  exact h1,
+end
+lemma to_finsupp_predicate {α : Type u} (m : multiset α) (p : α → Prop) : 
+(∀x ∈ m, p x) ↔ (∀x ∈ (m.to_finsupp).support, p x) :=
+iff.intro
+  begin
+    intros h1 x h2,
+    rw [to_finsupp_support, mem_to_finset] at h2,
+    exact h1 _ h2,
+  end
+  begin
+    intros h1 x h2,
+    rw [to_finsupp_support] at h1,
+    have h3 : x ∈ to_finset m → p x,
+    from h1 x,
+    rw [mem_to_finset] at h3,
+    exact h3 h2,
+  end
 
 end multiset
