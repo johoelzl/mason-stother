e6e1eb353d3dbea93571f761b408bc4900472179
JWageM <jens.wagemaker@gmail.com>
2018-04-15 13:11:29 +0200

    put all the lemmas on poly over integral domain in a separate file

--- poly_over_field.lean
+++ poly_over_field.lean
@@ -18,6 +18,323 @@ variables {α : Type u}
 
 section field
 
+
+--Naming is incorrect --Belongs in poly over field
+lemma leading_coeff_inv_mul_monic_of_ne_zero [field α ] {x : polynomial α} (h : x ≠ 0) : monic ((C (leading_coeff x)⁻¹) * x) :=
+begin
+  unfold monic,
+  rw [leading_coeff_mul_eq_mul_leading_coef],
+  simp,
+  simp at h,
+  rw [←leading_coef_eq_zero_iff_eq_zero] at h,
+  exact inv_mul_cancel h
+end
+
+lemma leading_coeff_dvd [field α ] {x : polynomial α} : (C (leading_coeff x)) ∣ x :=
+begin
+  by_cases h1 : (x = 0),
+  {
+    simp *
+  },
+  {
+    simp [has_dvd.dvd],
+    fapply exists.intro,
+    exact (C (leading_coeff x)⁻¹)*x,
+    rw [←leading_coef_eq_zero_iff_eq_zero, ←ne.def] at h1,
+    rw [←mul_assoc, ←C_mul_C, mul_inv_cancel h1],
+    simp
+  }
+
+
+
+end
+
+open associated
+
+
+
+
+def make_monic [field α] (f : polynomial α) := if (f = 0) then 0 else (C (f.leading_coeff)⁻¹ * f)
+
+#check make_monic
+
+
+
+lemma monic_make_monic_of_ne_zero [field α] (f : polynomial α) (h : f ≠ 0) : monic f.make_monic :=
+begin
+  simp [make_monic, if_neg, *],
+  exact leading_coeff_inv_mul_monic_of_ne_zero h,
+end
+
+def monic_out [field α] (a : quot (polynomial α)) : polynomial α :=
+quot.lift_on a make_monic (assume f p h,
+  begin
+    have h1: associated f p,
+      from h,
+    rcases h1 with ⟨u, hu⟩,
+    have hu2: is_unit ↑u,
+      from is_unit_unit u,
+    by_cases hf : f = 0,
+    {
+      subst hf,
+      have hu3 := hu.symm,
+      rw mul_eq_zero_iff_eq_zero_or_eq_zero at hu3,
+      cases hu3,
+      {
+        simp [hu3] at *,
+        contradiction,
+      },
+      {
+        subst hu3,
+      }
+    },
+    {
+      by_cases hp : p = 0,
+      {
+        subst hp,
+        simp * at *,
+      },
+      {
+        have hp2 : ↑u * p ≠  0,
+        {
+          rw [ne.def, mul_eq_zero_iff_eq_zero_or_eq_zero],
+          rw not_or_distrib,
+          exact ⟨ne_zero_of_is_unit zero_ne_one hu2, hp⟩,
+        },
+        simp [make_monic, if_neg, *],
+        rw [leading_coeff_mul_eq_mul_leading_coef, mul_inv_eq, C_mul_C, mul_assoc],
+        rw [eq_C_leading_coeff_of_is_unit hu2] {occs := occurrences.pos [2]},
+        rw [←mul_assoc (C (leading_coeff ↑u)⁻¹), ←C_mul_C, inv_mul_cancel],
+        simp,
+        rw [ne.def, leading_coef_eq_zero_iff_eq_zero],
+        rw [ne.def, mul_eq_zero_iff_eq_zero_or_eq_zero, not_or_distrib] at hp2,
+        exact hp2.1,
+        rw [ne.def, leading_coef_eq_zero_iff_eq_zero],
+        exact hp,
+        rw [ne.def, leading_coef_eq_zero_iff_eq_zero],
+        rw [ne.def, mul_eq_zero_iff_eq_zero_or_eq_zero, not_or_distrib] at hp2, --Duplication
+        simp *,
+      }
+    }
+  end)
+
+
+#check monic_out
+
+lemma monic_monic_out_of_ne_zero [field α] (f : quot (polynomial α)) (h : f ≠ 0) : monic (monic_out f) :=
+begin
+  revert h,
+  apply quot.induction_on f,
+  intros a h,
+  apply monic_make_monic_of_ne_zero,
+  rw [ne.def, ←mk_eq_zero_iff_eq_zero],
+  exact h,
+end
+
+/-@[simp] theorem quot.out_eq {r : α → α → Prop} (q : quot r) : quot.mk r q.out = q :=
+classical.some_spec (quot.exists_rep q)-/
+
+
+lemma polynomial_fac [field α] (x : polynomial α) : ∃ c :  α, ∃ p : multiset (polynomial α), x = C c * p.prod ∧ (∀x∈p, irreducible x ∧ monic x)  :=
+begin
+  by_cases h1 : (x = 0),
+  {
+    fapply exists.intro,
+    exact 0,
+    fapply exists.intro,
+    exact 0,
+    simp [C_0, h1],
+  },
+  {
+    by_cases h2 : (is_unit x),
+    {
+      have h3: ∃c : α, x =  C c,
+      from is_constant_of_is_unit h2,
+      let c := some h3,
+      fapply exists.intro,
+      exact c,
+      fapply exists.intro,
+      exact 0,
+      simp,
+      exact some_spec h3
+    },
+    {
+      let f := some (unique_factorization_domain.fac h1 h2),
+      have h3 : x = f.prod ∧ (∀x∈f, irreducible x),
+      from some_spec (unique_factorization_domain.fac h1 h2),
+      have h3a : x = multiset.prod f,
+      from and.elim_left h3,
+      have h3b : ∀ (x : polynomial α), x ∈ f → irreducible x,
+      from and.elim_right h3,
+      fapply exists.intro,
+      exact (f.map leading_coeff).prod,
+      fapply exists.intro,
+      exact (f.map (λ y, (C (leading_coeff y)⁻¹ )*y) ),
+      constructor,
+      {
+        rw h3a,
+        rw [C_prod_eq_map_C_prod, multiset.map_map],--, ←multiset.prod_add
+
+        have h4 : ∀ g : multiset (polynomial α), (∀ (y : polynomial α), y ∈ g → irreducible y) → multiset.prod g =
+    C (multiset.prod (multiset.map leading_coeff g)) *
+      multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) g),
+      {
+        apply @multiset.induction _ (λ (g : multiset (polynomial α)),
+    (∀ (y : polynomial α), y ∈ g → irreducible y) →
+    multiset.prod g =
+      C (multiset.prod (multiset.map leading_coeff g)) *
+        multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) g)),
+        {
+          simp [multiset.map_zero, multiset.map_zero, multiset.zero_add (0 : multiset (polynomial α))]
+        },
+        {
+          intros a s h4 h5,
+          have h5a : ∀ (y : polynomial α), y ∈ s → irreducible y,
+          {
+            intros y h6,
+            have : y ∈ a::s,
+            {simp *},
+            exact h5 y this,
+          },
+          have h4a : multiset.prod s =
+    C (multiset.prod (multiset.map leading_coeff s)) *
+      multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s),
+          from h4 h5a,
+          have h6 : irreducible a,
+          {
+            apply h5 a,
+            simp,
+          },
+          have h7 : leading_coeff a ≠ 0,
+          {
+            have : a ≠ 0,
+            from and.elim_left h6,
+            rw [ne.def, leading_coef_eq_zero_iff_eq_zero],
+            assumption
+          },
+          clear h4,
+          simp only [multiset.map_cons, multiset.prod_cons, C_prod_eq_map_C_prod],
+          apply eq.symm,
+          calc C (leading_coeff a * multiset.prod (multiset.map leading_coeff s)) *
+      (C (leading_coeff a)⁻¹ * a *
+         multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s)) =
+         (C (leading_coeff a) * C (multiset.prod (multiset.map leading_coeff s)) ) *
+      (C (leading_coeff a)⁻¹) * a *
+         multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s) : by simp [C_mul_C, mul_assoc]
+         ... = (C (leading_coeff a) * (C (multiset.prod (multiset.map leading_coeff s))  *
+      (C (leading_coeff a)⁻¹))) * a *
+         multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s) : by simp [mul_assoc]
+          ... = (C (leading_coeff a) * ( (C (leading_coeff a)⁻¹) *
+      C (multiset.prod (multiset.map leading_coeff s)) )) * a *
+         multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s) : by rw [mul_comm (C (leading_coeff a)⁻¹) _]
+        ... = C (leading_coeff a) *  (C (leading_coeff a)⁻¹) *
+              (C (multiset.prod (multiset.map leading_coeff s))  * a) *
+                multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s) : by simp only [mul_assoc]
+        ... = C (leading_coeff a) *  (C (leading_coeff a)⁻¹) *
+              (a  * C (multiset.prod (multiset.map leading_coeff s))) *
+                multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s) : by rw [mul_comm _ a]
+         ... = C (leading_coeff a) *  (C (leading_coeff a)⁻¹) *
+              a  * (C (multiset.prod (multiset.map leading_coeff s)) *
+                multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) s)) : by simp only [mul_assoc]
+          ... = C (leading_coeff a) *  (C (leading_coeff a)⁻¹) *
+              a  * (multiset.prod s) : by rw ←h4a
+          ... = C ((leading_coeff a) * ((leading_coeff a)⁻¹)) *
+              a  * (multiset.prod s) : by simp [C_mul_C, mul_assoc]
+          ... = C 1 * a * (multiset.prod s) : by rw [mul_inv_cancel h7]
+         ... = _ : by simp,
+        }
+      },
+      have : multiset.prod f =
+      C (multiset.prod (multiset.map leading_coeff f)) *
+        multiset.prod (multiset.map (λ (y : polynomial α), C (leading_coeff y)⁻¹ * y) f),
+      from h4 f h3b,
+      rw [C_prod_eq_map_C_prod, multiset.map_map] at this,
+      exact this,
+      },
+      intros y h1,
+      constructor,
+      {
+         --Must make lemmas for irreducible
+         rw multiset.mem_map at h1,
+         let a := some h1,
+         have h4 : a ∈ f ∧ C (leading_coeff a)⁻¹ * a = y,
+         from some_spec h1,
+         have h4a : a ∈ f,
+         from and.elim_left h4,
+         have h5 : irreducible a,
+         from h3b a h4a,
+         have h4b : C (leading_coeff a)⁻¹ * a = y,
+         from and.elim_right h4,
+         have h6 : is_unit (C (leading_coeff a)⁻¹),
+         {
+           apply is_unit_of_is_unit,
+           apply for_all_is_unit_of_not_zero,
+           apply inv_ne_zero,
+           rw [ne.def, leading_coef_eq_zero_iff_eq_zero],
+           exact h5.1,
+         },
+         have h7 : (y ~ᵤ a),
+         {
+           rcases h6 with ⟨u, hu⟩,
+           exact ⟨u, by simp * at *⟩,
+
+         },
+         have h8 : (a ~ᵤ y),
+         from associated.symm h7,
+         exact irreducible_of_associated h5 h8
+      },
+      {
+        rw multiset.mem_map at h1,
+        let a := some h1,
+        have h4 : a ∈ f ∧ C (leading_coeff a)⁻¹ * a = y,
+        from some_spec h1,
+        have h4a : C (leading_coeff a)⁻¹ * a = y,
+        from and.elim_right h4,
+        rw ←h4a,
+        apply leading_coeff_inv_mul_monic_of_ne_zero,
+        have h4b : a ∈ f,
+        from and.elim_left h4,
+        exact and.elim_left (h3b a h4b)
+      }
+    }
+
+  }
+end
+
+
+
+/-
+--The more general setting, avoids problem with the roots of the zero polynomial
+axiom monic_irr (p : polynomial β) : polynomial β →₀ ℕ
+axiom irr_poly_irreducible (p : polynomial β) : ∀x ∈ (monic_irr p).support, irreducible x
+axiom irr_poly_monic (p : polynomial β) : ∀x ∈ (monic_irr p).support, monic x
+axiom unique_factorization (p : polynomial β) : ∃ c : β , p = C c * ((finsupp.prod (monic_irr p) (λ k n, k ^n) ) )
+def c_fac (p : polynomial β) : β := some ( unique_factorization p)
+axiom c_fac_unit (p : polynomial β) :  is_unit (c_fac p)
+-/
+
+lemma polynomial_fac_finsupp [field α] (x : polynomial α) : ∃ c :  α, ∃ p :(polynomial α) →₀ ℕ, x = C c * ((finsupp.prod (p) (λ k n, k ^n) ) ) ∧ (∀x∈p.support, irreducible x ∧ monic x)  :=
+begin
+  have h1 : ∃ c :  α, ∃ p : multiset (polynomial α), x = C c * p.prod ∧ (∀x∈p, irreducible x ∧ monic x),
+  from polynomial_fac _,
+  rcases h1 with ⟨c, p, h2⟩,
+  exact ⟨c, p.to_finsupp,
+    begin
+      rw ←multiset.to_finsupp_prod,
+      constructor,
+      exact h2.1,
+      intros y h3,
+      rw multiset.to_finsupp_support at h3,
+      let h4 := h2.2,
+      rw multiset.mem_to_finset at h3,
+      exact h4 _ h3,
+      end
+  ⟩
+end
+
+
+
+
 variable [field α]
 
 lemma is_unit_C_of_ne_zero {c : α} (h : c ≠ 0) : is_unit (C c) :=
@@ -264,6 +581,8 @@ begin
     subst h2,
   }
 end
+
+
 
  
 end field
